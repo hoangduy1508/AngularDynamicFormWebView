d9123687d87ff9072c14b0afe047fd73dbc2a8dd
hoangduy <duy.unh@mapmaker.asia>
2022-05-17 14:00:44 +0700

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularDynamicFormWebView';
 
-  showAndroidToast(toast) {
-    Android.showToast(toast);
+  formGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ){
+
+  }
+
+  ngOnInit(): void {
+      this.generateFormGroup();
+  }
+
+  private generateFormGroup(): void {
+    this.formGroup = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      gender: [''],
+      age: [''],
+    });
+  }
+
+  submitForm(){
+    console.log(this.formGroup.value);
+
+    this.showAndroidToast(this.formGroup.value);
+  }
+
+  showAndroidToast(submitData: any) {
+    Android.onSubmitFormSuccess(submitData);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
