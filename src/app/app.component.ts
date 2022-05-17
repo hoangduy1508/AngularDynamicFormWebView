8a83c96be197a4bf0a9bea73a7ff90c35ca63591
hoangduy <duy.unh@mapmaker.asia>
2022-05-17 13:34:56 +0700

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularDynamicFormWebView';
+
+  showAndroidToast(toast) {
+    Android.showToast(toast);
+  }
 }
